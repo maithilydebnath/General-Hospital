2650e997ac5083d16a94757ab17829fa43f1eeec
Maithily <maithilydebnath@gmail.com>
2021-10-18 12:01:02 +1100

    Banner Add

---
+++ src/components/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h6> General Hospital</h6>
+            <p>©General Hospital 2021,We are always here to help you.</p>
+        </div>
+    );
+};
+
+export default Footer;
